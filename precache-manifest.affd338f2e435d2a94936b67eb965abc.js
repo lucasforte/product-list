a4a5f793cf41895d6bd8d197b72d8ddd0002f58e
Lucas Forte <lucasforte92@gmail.com>
2020-07-22 15:23:45 -0300

    Stable version

---
+++ precache-manifest.affd338f2e435d2a94936b67eb965abc.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb080dea7015dd39eafbd2f6490a11d6",
+    "url": "/product-list/index.html"
+  },
+  {
+    "revision": "bda5f7a539de961cd191",
+    "url": "/product-list/static/css/main.aea3639a.chunk.css"
+  },
+  {
+    "revision": "5a4b868c974c18d9ef1d",
+    "url": "/product-list/static/js/2.38e48e3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/product-list/static/js/2.38e48e3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bda5f7a539de961cd191",
+    "url": "/product-list/static/js/main.ce89d54a.chunk.js"
+  },
+  {
+    "revision": "c58387e07571e3c54eee",
+    "url": "/product-list/static/js/runtime-main.b0309d5b.js"
+  },
+  {
+    "revision": "030ae8dd14e3c9e62d9ccc78e8460763",
+    "url": "/product-list/static/media/header.030ae8dd.jpg"
+  },
+  {
+    "revision": "09bbc186e8ba2ede67909fcce63be79c",
+    "url": "/product-list/static/media/kingkiller.09bbc186.jpg"
+  },
+  {
+    "revision": "618c53f0d665ad003b2cf1f511a2fe39",
+    "url": "/product-list/static/media/neverwhere.618c53f0.jpg"
+  },
+  {
+    "revision": "948a4a8965ee6978f158fa95750ebaab",
+    "url": "/product-list/static/media/promethea-1.948a4a89.jpg"
+  },
+  {
+    "revision": "15868c79111c10d1e9af08cfb17bfa1b",
+    "url": "/product-list/static/media/promethea-2.15868c79.jpg"
+  },
+  {
+    "revision": "66c6939318783c432d2717911170205f",
+    "url": "/product-list/static/media/sandman-1.66c69393.jpg"
+  },
+  {
+    "revision": "8d0abb7b8c910a844d17d8274a10b056",
+    "url": "/product-list/static/media/sandman-2.8d0abb7b.jpg"
+  },
+  {
+    "revision": "eb3d949016f504edd5d0697b1a62bda8",
+    "url": "/product-list/static/media/slow-regard.eb3d9490.jpg"
+  },
+  {
+    "revision": "62d1de5934430978f3156afd126087d9",
+    "url": "/product-list/static/media/stardust.62d1de59.jpg"
+  },
+  {
+    "revision": "a9d55cc57d184c957c2089a87b813a85",
+    "url": "/product-list/static/media/tlotr.a9d55cc5.jpg"
+  }
+]);
